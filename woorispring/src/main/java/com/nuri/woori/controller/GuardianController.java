39943f9f3e3db7599a5a753a094255533c00387c
dnfqhrjsgml <dnfqhrjsgml@gmail.com>
2026-06-02 11:46:31 +0900

    Fix guardian login merge regression

--- woorispring/src/main/java/com/nuri/woori/controller/GuardianController.java
+++ woorispring/src/main/java/com/nuri/woori/controller/GuardianController.java
@@ -70,16 +70,15 @@ public class GuardianController {
             return ResponseEntity.status(404).body(Map.of("message", "EMAIL_NOT_FOUND"));
         }
 
-        if (Boolean.FALSE.equals(guardian.getActive())) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Inactive account");
-        }
-
-        return toResponse(guardian);
-
         Guardian guardian = guardianOpt.get();
         if (!guardian.getPassword().equals(hashPassword(request.password()))) {
             return ResponseEntity.status(401).body(Map.of("message", "PASSWORD_MISMATCH"));
         }
+
+        if (Boolean.FALSE.equals(guardian.getActive())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Inactive account");
+        }
+
         return ResponseEntity.ok(toResponse(guardian));
     }
 
