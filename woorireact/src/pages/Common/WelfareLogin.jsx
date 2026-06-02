076b400c9524e935c00ad837de1bea81390f1e3e
dnfqhrjsgml <dnfqhrjsgml@gmail.com>
2026-06-02 16:23:11 +0900

    Fix welfare API proxy configuration

--- woorireact/src/pages/Common/WelfareLogin.jsx
+++ woorireact/src/pages/Common/WelfareLogin.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import "../../css/common/Login.css";
 
-const API_BASE = "http://localhost:8080";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "";
 
 const welfareFeatures = [
   {
